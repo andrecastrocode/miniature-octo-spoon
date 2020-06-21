98c8b0cbd47ae4ef54854a8f31ea8dbe0ea43b15
André Castro <33205006+andrecastrocode@users.noreply.github.com>
2020-06-21 23:22:54 +0100

    Add Boost and QT Core;

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,17 @@
 
 #include <iostream>
 
-int main() {
+#include <boost/version.hpp>
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[]) {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "BOOST VERSION: " << BOOST_LIB_VERSION << std::endl;
+    std::cout << "QT VERSION: " << QT_VERSION_STR << std::endl;
+
+    QCoreApplication app(argc, argv);
+    QCoreApplication::exec();
+
     return 0;
 }
